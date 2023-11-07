b2584d3c754a58f57dd05b859f213658e94f5220
matgus217 <>
2023-11-07 14:06:14 +0000

    added some style in css

---
+++ assets/img/cards.js
@@ -0,0 +1,47 @@
+[
+    {
+        "image": "../assets/images/goat.png",
+        "name": "goat"
+    },
+
+    {
+        "image": "../assets/images/cat.png",
+        "name": "cat"
+
+    },
+
+    {
+        "image": "../assets/images/dog.png",
+        "name": "dog"
+    },
+
+    {
+        "image": "../assets/images/shark.png",
+        "name": "shark"
+    },
+
+    {
+        "image": "../assets/images/bird.png",
+        "name": "bird"
+    },
+
+    {
+        "image": "../assets/images/cow.png",
+        "name": "cow"
+    },
+
+    {
+        "image": "../assets/images/horse.png",
+        "name": "horse"
+    },
+
+    {
+        "image": "../assets/images/rabbit.png",
+        "name": "rabbit"
+    },
+
+    {
+        "image": "../assets/images/pig.png",
+        "name": "pig"
+    },
+]
