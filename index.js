933d81be61a56435b88cabf204ea2b46978bdfad
matgus217 <>
2023-11-15 14:22:46 +0000

    Change styles in index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const finalScore = document.getElementById("finalPoints");
 const won = document.getElementById("won");
 const play = document.getElementById("playAgain");
 const button = document.getElementsByClassName("btn-handle");
-const ship = document.getElementById("ship");
+
 const body = document.getElementsByTagName("body")[0];
 
 var points = 0;
@@ -91,7 +91,6 @@ play.addEventListener("click", playAgain);
 cards.forEach((card) => card.addEventListener("click", flipCard));
 
 
-function shipMove() {
-    ship.classList.add = "animate__slideOutRight";
-}
+
+
 button.addEventListener("click", shipMove);
